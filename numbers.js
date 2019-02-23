e4b0931b38c9c18fc8e60cf7e1b4f41c1e79dce4
Heloise Taillet <heloise96@hotmail.com>
2019-02-23 12:49:36 -0500

    Done.

--- numbers.js
+++ numbers.js
@@ -1,1 +1,68 @@
 // Write your numbers code in this file!
+let year = 2019;
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById(`number-one`).addEventListener('click', oneFacts)
+  document.getElementById(`pick-a-number`).addEventListener('change', fetchNumber)
+  fetchYear(year)
+  setInterval(fetchYear, 5000)
+  document.getElementById(`all-numbers-button`).addEventListener('click', fetchHundred)
+})
+
+function fetchHundred() {
+    for (i = 0; i < 99; i++) {
+  let random = Math.floor((Math.random() * 10000000) + 1);
+  fetch(`http://numbersapi.com/${random}/trivia`)
+  .then(res => res.text())
+  .then(fact=> renderHundred(fact))
+    }
+}
+
+function renderHundred(fact) {
+  let node = document.createElement("p");
+  node.innerHTML = fact;
+  document.querySelector("#all-the-numbers").appendChild(node)
+}
+
+function fetchYear() {
+  fetch(`http://numbersapi.com/${--year}/trivia`)
+  .then(res => res.text())
+  .then(fact=> renderYearlyFact(fact))
+}
+
+function renderYearlyFact(fact) {
+  document.querySelector("#year-history").innerHTML = ""
+  let node = document.createElement("p");
+  node.innerHTML = fact;
+  document.querySelector("#year-history").appendChild(node)
+}
+
+function oneFacts() {
+  fetch('http://numbersapi.com/1/trivia')
+  .then(res => res.text())
+  .then(fact=> renderOne(fact))
+}
+
+function renderOne(fact){
+  document.querySelector("#one-facts").innerHTML = ""
+  let node = document.createElement("p");
+  node.innerHTML = fact;
+  document.querySelector("#one-facts").appendChild(node)
+}
+
+function fetchNumber(fact){
+  if (!isNaN(fact.target.value)) {
+    fetch(`http://numbersapi.com/${fact.target.value}/trivia`)
+    .then(res => res.text())
+    .then(fact=> renderNumber(fact))
+  } else {
+    renderNumber("That's not a number. Try again.")
+  }
+}
+
+function renderNumber(fact){
+  document.querySelector("#random-math-fact").innerHTML = ""
+  let node = document.createElement("p");
+  node.innerHTML = fact;
+  document.querySelector("#random-math-fact").appendChild(node)
+}
--- swapi.js
+++ swapi.js
@@ -1,1 +1,51 @@
 // Write your swapi code in this file!
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById(`crawlBtn`).addEventListener('click', fetchCrawl)
+  document.getElementById(`planetForm`).addEventListener('submit', fetchPlanets)
+  fetchDroid(2)
+  fetchDroid(3)
+ })
+
+function fetchDroid(num) {
+  fetch(`https://swapi.co/api/people/${num}`)
+  .then(res => res.json())
+  .then(json => {
+    renderDroid(json,num)
+  });
+}
+
+function renderDroid(droid,num) {
+  document.querySelector(`#droid-${num}`).innerHTML = `Name: ${droid.name} Height: ${droid.height} Mass: ${droid.mass}`
+}
+
+function fetchCrawl() {
+  fetch('https://swapi.co/api/films/1/')
+  .then(res => res.json())
+  .then(json => {
+    renderCrawl(json.opening_crawl)
+  });
+}
+
+function renderCrawl(crawl) {
+  document.querySelector("#crawlDiv").innerHTML = ""
+  let opening = document.createElement("P");
+  opening.innerHTML = crawl;
+  document.querySelector("#crawlDiv").appendChild(opening)
+}
+
+function fetchPlanets(e) {
+  e.preventDefault()
+  fetch(`https://swapi.co/api/planets/${e.target.children[0].value}`)
+  .then(res => res.json())
+  .then(json => {
+    renderPlanets(json)
+  })
+}
+
+function renderPlanets(planet) {
+  document.querySelector("#planetData").innerHTML = ""
+  let node = document.createElement("P");
+  node.innerHTML = planet.name;
+  document.querySelector("#planetData").appendChild(node)
+}
